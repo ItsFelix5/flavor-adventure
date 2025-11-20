7c1c6538c90f11c5cfda4f5158214c8bdf7f486e
EDRipper <euan@hackclub.com>
2025-11-20 13:36:42 -0500

    fix: allow CORS in maps/flavor and use dynamic host in redirect script

--- maps/flavor/redirect-to-house.js
+++ maps/flavor/redirect-to-house.js
@@ -7,9 +7,17 @@ WA.room.onEnterZone('redirectToOffice', () => {
     
     const slackId = WA.player.slackId;
     console.log('Slack ID:', slackId);
+    console.log('Current Host:', window.location.host);
     
     if (slackId) {
-        const targetUrl = `/_/global/${window.location.host}/slack/${slackId}`;
+        let targetHost = 'flavor-adventure.hackclub.com';
+        
+        // Check if we are in dev environment
+        if (window.location.host.includes('workadventure.localhost') || window.location.host.includes('localhost')) {
+            targetHost = window.location.host;
+        }
+
+        const targetUrl = `/_/global/${targetHost}/slack/${slackId}`;
         console.log('Sending to house', targetUrl);
         WA.nav.goToRoom(targetUrl);
     } else {
